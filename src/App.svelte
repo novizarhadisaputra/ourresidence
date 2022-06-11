1d94ecaed5e67764e7478c3f1515e689115233af
unknown <novizarhadisaputra@gmail.com>
2022-06-11 10:22:41 +0700

    First commit and installation flowbite-svelte

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { AccordionItem } from "flowbite-svelte";
+</script>
+
+<main>
+  <AccordionItem id="1">
+    <h2 slot="header">My Header 1</h2>
+    <div slot="body">
+      <p class="mb-2 text-gray-500 dark:text-gray-400">
+        Lorem ipsum dolor sit amet ...
+      </p>
+      ...
+    </div>
+  </AccordionItem>
+  <AccordionItem id="2">
+    <h2 slot="header">My Header 2</h2>
+    <div slot="body">
+      <p class="mb-2 text-gray-500 dark:text-gray-400">
+        Lorem ipsum dolor sit amet ...
+      </p>
+      ...
+    </div>
+  </AccordionItem>
+</main>
+
+<style>
+</style>
